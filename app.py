f8c0fdcf4c260831c8d469acff4088ec24671686
Kirshoff <fatihfidan52@gmail.com>
2021-01-17 16:49:56 +0100

    update

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+# http://127.0.0.1:5000/users/2 -> Base Url
+@app.route('/home')
+def index():
+    return 'Hello Flask'
+@app.route('/users/<user_id>' , methods = ['GET','POST','PUT','DELETE'])
+def parameter(user_id):
+    if request.method == 'GET':
+        return 'GET method'
+
+    elif request.method == 'PUT':
+        return 'PUT method'
+
+    elif request.method == 'POST':
+        data=request.form
+        return 'POST method'
+
+    elif request.method == 'DELETE':
+        return 'DELETE method'
+
+    else: 'Not allowed method'
+    return user_id
+
+app.run()
+
+
+
+
